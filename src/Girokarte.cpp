c4d3ff4a6bb32362a6dc636eab79f58b643378ae
BennyWi <156936343+BennyWi@users.noreply.github.com>
2024-03-14 10:22:44 +0100

    generatPIN

--- src/Girokarte.cpp
+++ src/Girokarte.cpp
@@ -1,6 +1,12 @@
 #include "Girokarte.h"
+#include <chrono>
+#include <random>
+#include <iostream>
 
-Girokarte::Girokarte() : PIN(0) {
+
+
+
+Girokarte::Girokarte() : PIN(Girokarte::generatPIN()) {
   this->userPIN = 0;
   this->gesperrt = false;
   this->KARTEN_TYP = Kartentypen::GIROKARTE;
@@ -8,10 +14,26 @@ Girokarte::Girokarte() : PIN(0) {
 }
 
 bool Girokarte::checkKarte() {
-  // return userPIN == PIN;
-  return true;
+  std::cout << "Deine PIN lautet: " << PIN << std::endl;
+  setUserPIN();
+  return userPIN == PIN;
+  
 }
 
-void Girokarte::setUserPIN(unsigned int pin) {
-  this->userPIN = pin;
+int Girokarte::generatPIN() {
+  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+  std::knuth_b genRand(seed);
+  std::uniform_int_distribution<int> w10{1000, 9999};
+  
+  return w10(genRand);
+  
+}
+
+void Girokarte::setUserPIN() {
+  std::cout << "Bitte geben Sie den PIN ein: " << std::endl;
+  int pinEingabe = 0;
+  std::cin >> pinEingabe;
+
+  this->userPIN = pinEingabe;
+
 }
